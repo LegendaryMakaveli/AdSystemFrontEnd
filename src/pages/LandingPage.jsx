d8af638656369f872b8b8c84b91646463fcafd6e
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-12-24 14:58:26 +0100

    landing page

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,44 @@
+import style from "../pages/LandingPage.module.css"
+
+const LandingPage = () => {
+    return (
+    <>
+        <div className={style.navContainer}>
+            <div className={style.topBar}>
+                <h1>Makaveli</h1>
+                <div className={style.topBarLinks}>
+                    <ul>
+                        <li>Get The App</li>
+                        <li>My Listing</li>
+                        <li>Create A Listing</li>
+                    </ul>
+                </div>
+                <div className={style.icons}>
+                    <span className="material-symbols-outlined">language</span>
+                    <span className="material-symbols-outlined">account_circle</span>
+                    <span className="material-symbols-outlined">favorite</span>
+                </div>
+            </div>
+            <div className={style.smallNavLinks}>
+                <ul>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Services</li>
+                    <li>Gigs</li>
+                    <li>Commuity</li>
+                </ul>
+            </div>
+
+            <div className={style.message}>
+                <div className={style.messageInput}>
+                    <h1>Welcome To Makaveli Hub</h1>
+                    <p>Post your business for the world to see</p>
+                    <button className={style.login}>Login to Continue</button>
+                    <button className={style.signup}>SignUp to Continue</button>
+                </div>
+            </div>
+        </div>
+    </>
+    )
+}
+export default LandingPage
